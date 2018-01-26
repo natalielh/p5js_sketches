9af08eff637dd1fb42e88c487dde13fef2e46554
natalielh <nlehuenen@gmail.com>
2018-01-26 16:15:18 -0500

    updated utilities and added sketches
    - updated p5utilities.js (you can now press SPACEBAR or R to reload the page)
    - added more sketches

--- javascript/p5utilities.js
+++ javascript/p5utilities.js
@@ -12,12 +12,25 @@ function keyPressed(){
 
 	// //DEACTIVATE FULLSCREEN MODE
 	// case 27: //keyCode 27 is 'escape' on keyboard
-	// 	fullScreen(false);
-	// 	document.exitFullscreen();
-	// 	break;
+	// fullScreen(false);
+	// document.exitFullscreen();
+	// break;
+
+	//RELOAD THE PAGE
+	case 32: //keyCode 32 is 'SPACEBAR' on keyboard
+	case 82: //keyCode 82 is 'r' on keycoard
+		window.location.reload(false); 
+		// If we needed to pull the document from
+		// the web-server again (such as where the document contents
+		// change dynamically) we would pass the argument as 'true'.
+		break;
 
 	//IF THE KEY PRESSED DOESN'T MATCH ANY OF THE ABOVE:
 	default: 
 		return false;
 	}
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
